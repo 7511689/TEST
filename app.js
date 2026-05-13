c0e14e45214e549093574269dc9800070f708866
7511689 <nseve751@gmail.com>
2026-05-13 22:09:38 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -426,8 +426,7 @@ window.playOneClick = function(platform) {
             ios: 'https://tinyurl.com/4pnmfykf'
         },
         'genie': {
-            // tinyurl 대신 지니 앱을 바로 여는 실제 딥링크 주소를 넣습니다.
-            android: 'cromegenie://scan/?landing_type=31&landing_target=115286596;115286597;115286599;115286598;115286600;115286601;115286596;115286597;112903134;115286599;115286598;115286596;115286600;115286601;112903134;115286597;115286596;115286599;115286598;115286600;115286601;112903134',
+            android: 'https://tinyurl.com/3dpkjrmv',
             ios: 'https://tinyurl.com/mpa9euph',
             pc: 'https://tinyurl.com/v7ss7np6'
         },
