6f06e133460914c61a305f38e0fb76d7477a7dc7
7511689 <nseve751@gmail.com>
2026-05-15 22:42:38 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -93,8 +93,7 @@ const pages = {
             <div class="chart-header">
                 <span class="chart-badge">실시간 차트</span>
                 <button class="chart-copy-btn" id="chart-copy-btn" onclick="copyChartReport()" title="리포트 복사">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
-                    리포트 복사
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
                 </button>
             </div>
             <div class="chart-cards">
